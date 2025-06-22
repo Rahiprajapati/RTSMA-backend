e6baea6bdce01bf48e41f59bd00ee92ca3e8307f
Rahi prajapati <rahiprajapati1807@gmail.com>
2025-06-22 13:59:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default_secret")
 
     # MySQL Configuration from environment
-    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_HOST = os.getenv("host="mysql-xyz.render.com")
     MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
     MYSQL_USER = os.getenv("MYSQL_USER",'root')
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD",'Rahi1807@')
